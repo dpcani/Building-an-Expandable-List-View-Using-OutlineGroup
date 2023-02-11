f8dd8d2d4464b478c22c9b625d49f11c63d2a66d
David de la Puente <david.puente@marbleinteractive.com>
2023-02-11 08:40:33 +0100

    Create a List with header and subitems with hierachy of data

--- SwiftUIExpandableList/SwiftUIExpandableList/ContentView.swift
+++ SwiftUIExpandableList/SwiftUIExpandableList/ContentView.swift
@@ -9,23 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        List(sampleMenuItems, children: \.subMenuItems) { item in
-            HStack {
-                Image(item.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 50, height: 50)
-                
-                Text(item.name)
-                    .font(.system(.title3, design: .rounded))
-                    .bold()
+        List {
+            ForEach(sampleMenuItems) { menuItem in
+                Section(header:
+                HStack {
+                    Text(menuItem.name)
+                        .font(.title3)
+                        .fontWeight(.heavy)
+                    Image(menuItem.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 30, height: 30)
+                }
+                .padding(.vertical)
+                ){
+                    OutlineGroup(menuItem.subMenuItems ?? [MenuItem](), children: \.subMenuItems) {  item in
+                        HStack {
+                            Image(item.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                            Text(item.name)
+                                .font(.system(.title3, design: .rounded))
+                                .bold()
+                        }
+                    }
+                }
             }
-            
         }
-        .listStyle(.plain)
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
